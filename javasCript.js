0cd6df659165fb965f886159a59587cbda9681a8
Hidemi2008 <Gabrielhidemi2@gmail.com>
2024-11-11 12:17:10 -0400

    Create html and javasCript

---
+++ javasCript.js
@@ -0,0 +1,18 @@
+let library = ["The good father ", "Harry Potter ", "Screen ", "The good Fellas "]
+let block = document.querySelector("#block")
+
+function addBlock(){
+    for(let i = 0; i < library.length; i++){
+        
+        block.innerHTML +=  library[i]
+    }
+}
+
+
+function book(){
+
+}
+
+function addBookToLibrary(){
+
+}
